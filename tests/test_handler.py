c34ed7b4bb638fef71d9a4ff74d2309414e317be
Kyle Tennison <kyletennison05@gmail.com>
2023-11-29 23:16:16 -0800

    cleanup test handler

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -1,66 +1,102 @@
 """
-Tools for running deployment tests.
+Tools for running deployment tests
 
+Kyle Tennison
 August 2023
 """
 
 
 import sys
 import os
-
-parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-sys.path.append(parent_dir)
-
 import threading
 import time
 import cloud_manager
 
+# Add cloud_manager to sys path
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(parent_dir)
 
-def main():
-    """Runs on import"""
+class TestHandler:
 
-    # Change to api-server directory
-    path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "..")
-    os.chdir(path)
+    def __init__(self) -> None:
+        # Change to api-server directory
+        path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "..")
+        os.chdir(path)
 
+    @classmethod
+    def start_server(cls) -> None:
+        """Starts the server daemon thread"""
 
-def start_server():
-    """Starts the server in a daemon-style"""
+        def worker():
+            cloud_manager.webserver.main()
 
-    def worker():
-        cloud_manager.webserver.main()
+        threading.Thread(target=worker, daemon=True).start()
 
-    threading.Thread(target=worker, daemon=True).start()
+        print("Waiting for server to start...")
+        time.sleep(5)
+        print("Assuming server has started")
 
-    print("Waiting for server to start...")
-    time.sleep(5)
-    print("Assuming server has started")
+    @classmethod
+    def stop_server(cls) -> None:
+        """Kills the server thread by exiting the program"""
+        exit()
 
+    @classmethod
+    def make_url(cls, endpoint: str) -> str:
+        """Converts a endpoint into a localhost url to the test server
+        
+        Args:
+            endpoint: The endpoint to convert into an url
+        
+        Returns:
+            The url created by the endpoint
 
-def stop_server():
-    exit()
+        """
+        return f"http://localhost:3001{endpoint}"
 
+    @classmethod
+    def message(cls, message: str) -> None:
+        """Prints a generic testing message
+        
+        Args:
+            message: The message to print
+        """
+        print(f"\033[92m{message}\x1b[0m")
 
-def make_url(endpoint):
-    return f"http://localhost:3001{endpoint}"
+    @classmethod
+    def warn(cls, message: str) -> None:
+        """Prints a warning (yellow) message
+        
+        Args:
+            message: The warning to print
+        """
+        print(f"\033[93m {message}\033[00m")
 
+    @classmethod
+    def report(cls, message: str, test_name: str) -> None:
+        """Reports an error to the test handler. Shuts down server and
+            exits test
 
-def message(message):
-    print(f"\033[92m{message}\x1b[0m")
+        Args:
+            message: The message to report
+            test_name: The name of the test that is being reported
+        
+        """
+        print(f"\033[91m Test {test_name} failed: {message}\033[0m")
+        sys.stderr.write(message)
+        exit(1)
 
+    @classmethod
+    def testpass(cls, test_name: str, no_shutdown: bool) -> None:
+        """Signals that a test has passed. Shuts down server and
+            exits test.
+        
+        Args:
+            test_name: The name of the test that passed
+            no_shutdown: Prevent the server from shutting down and
+                the test from exiting
+        """
+        print(f"\033[92m \n\nTEST '{test_name.upper()}' PASSED \x1b[0m")
 
-def warn(message):
-    print(f"\033[93m {message}\033[00m")
-
-
-def report(message, testname):
-    print(f"\033[91m Test {testname} failed: {message}\033[0m")
-    sys.stderr.write(message)
-    exit(1)
-
-
-def testpass(testname):
-    print(f"\033[92m \n\nTEST '{testname.upper()}' PASSED \x1b[0m")
-
-
-main()
+        if not no_shutdown:
+            exit(0)
